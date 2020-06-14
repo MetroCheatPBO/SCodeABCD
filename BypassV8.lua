43bf3f1496ba10b032977adb91a105c053c6beb7
MetroCheatPBO <64860864+MetroCheatPBO@users.noreply.github.com>
2020-06-14 22:01:49 +0700

    Create BypassV8.lua

---
+++ BypassV8.lua
@@ -0,0 +1,170 @@
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("67109633", gg.TYPE_DWORD)
+gg.getResults(50000)
+gg.editAll("0", gg.TYPE_DWORD)
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("67109633", gg.TYPE_DWORD)
+gg.getResults(50000)
+gg.editAll("0", gg.TYPE_DWORD)
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("67109377;12547::100", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("12547", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+jg = gg.getResults(100)
+sl = gg.getResultCount()
+if 100 < sl then
+  sl = 100
+end --MCPBO™--
+for i = 1, sl do
+  dzy = jg[i].address
+  gg.addListItems({
+    [1] = {
+      address = dzy,
+      flags = gg.TYPE_DWORD,
+      freeze = true,
+      value = 0
+    }
+  })
+end --MCPBO™--
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("0;0~20;131586::9", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("131586", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+jg = gg.getResults(100)
+sl = gg.getResultCount()
+if 100 < sl then
+  sl = 100
+end --MCPBO™--
+for i = 1, sl do
+  dzy = jg[i].address
+  gg.addListItems({
+    [1] = {
+      address = dzy,
+      flags = gg.TYPE_DWORD,
+      freeze = true,
+      value = 0
+    }
+  })
+end --MCPBO™--
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("67109377;12547::100", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("12547", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+jg = gg.getResults(100)
+sl = gg.getResultCount()
+if 100 < sl then
+  sl = 100
+end --MCPBO™--
+for i = 1, sl do
+  dzy = jg[i].address
+  gg.addListItems({
+    [1] = {
+      address = dzy,
+      flags = gg.TYPE_DWORD,
+      freeze = true,
+      value = 0
+    }
+  })
+end --MCPBO™--
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("0;0~20;131586::9", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("131586", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+jg = gg.getResults(100)
+sl = gg.getResultCount()
+if 100 < sl then
+  sl = 100
+end --MCPBO™--
+for i = 1, sl do
+  dzy = jg[i].address
+  gg.addListItems({
+    [1] = {
+      address = dzy,
+      flags = gg.TYPE_DWORD,
+      freeze = true,
+      value = 0
+    }
+  })
+end --MCPBO™--
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("67109377;12547::100", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("12547", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+jg = gg.getResults(100)
+sl = gg.getResultCount()
+if 100 < sl then
+  sl = 100
+end --MCPBO™--
+for i = 1, sl do
+  dzy = jg[i].address
+  gg.addListItems({
+    [1] = {
+      address = dzy,
+      flags = gg.TYPE_DWORD,
+      freeze = true,
+      value = 0
+    }
+  })
+end --MCPBO™--
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("0;0~20;131586::9", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("131586", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+jg = gg.getResults(100)
+sl = gg.getResultCount()
+if 100 < sl then
+  sl = 100
+end --MCPBO™--
+for i = 1, sl do
+  dzy = jg[i].address
+  gg.addListItems({
+    [1] = {
+      address = dzy,
+      flags = gg.TYPE_DWORD,
+      freeze = true,
+      value = 0
+    }
+  })
+end --MCPBO™--
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("67109377;12547::100", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("12547", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+jg = gg.getResults(100)
+sl = gg.getResultCount()
+if 100 < sl then
+  sl = 100
+end --MCPBO™--
+for i = 1, sl do
+  dzy = jg[i].address
+  gg.addListItems({
+    [1] = {
+      address = dzy,
+      flags = gg.TYPE_DWORD,
+      freeze = true,
+      value = 0
+    }
+  })
+end --MCPBO™--
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("0;0~20;131586::9", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("131586", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+jg = gg.getResults(100)
+sl = gg.getResultCount()
+if 100 < sl then
+  sl = 100
+end --MCPBO™--
+for i = 1, sl do
+  dzy = jg[i].address
+  gg.addListItems({
+    [1] = {
+      address = dzy,
+      flags = gg.TYPE_DWORD,
+      freeze = true,
+      value = 0
+    }
+  })
+end --MCPBO™--
+gg.alert("BYPASS MCPBO V8 DONE")
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("67109377", gg.TYPE_DWORD)
+gg.getResults(50000)
+gg.editAll("0", gg.TYPE_DWORD)
+gg.clearResults()
